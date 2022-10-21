24ee3c963e72ffc55d7336f8397a48e45ace1186
Int3rst3ll3r <76830425+Int3rst3ll3r@users.noreply.github.com>
2022-10-21 13:37:31 +0530

    files added peak element completed

--- Atinder/src/com/Atinder/GfgArray/PeakElement2.java
+++
@@ -1,6 +0,0 @@
-package com.Atinder.GfgArray;
-
-public class PeakElement2 {
-  
-	 
-}
--- Atinder/src/com/Atinder/GfgArray/peakElement.java
+++ Atinder/src/com/Atinder/GfgArray/peakElement.java
@@ -7,20 +7,40 @@ public class peakElement {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int arr[]= new int[10];
-		System.out.println("enter the number of th elements");
-		int n = scan.nextInt();
-		System.out.println("enter the array elements ");
-		for(int i=0;i<n;i++) {
-			arr[i]=scan.nextInt();
-		}
+		int arr[]= new int[] {2,5,6,3,7,9,5,3,8};
+//		System.out.println("enter the number of the elements");
+//		int n = scan.nextInt();
+//		System.out.println("enter the array elements ");
+//		for(int i=0;i<n;i++) {
+//			arr[i]=scan.nextInt();
+//		}
+		int  n=arr.length;
 		System.out.println("The array is ");
 		System.out.println(Arrays.toString(arr));
 		int h =peakElement.peakele(arr,n);
 		System.out.println("the index of the peak element is "+ h);
+		int k =peakElement.peakele2(arr,n,0,n-1);
+		System.out.println("the index of the peak element is "+ k);
 		
 	
 	}
+	//Binary Search is correct one
+	static int peakele2(int arr[],int n,int low,int high ) {
+		while(low<=high) {
+		int mid=(low+high)/2;
+	   if((mid==0||arr[mid]>=arr[mid+1])&&(mid==n-1||arr[mid]>=arr[mid-1])) 
+		   return mid;
+	   else if((mid>0&&arr[mid]<arr[mid+1])) {
+	      return peakele2(arr,n,mid+1,high);
+	    	  }
+	   else {
+				
+		  return peakele2(arr,n,low,mid-1);
+			}
+		}
+		return 0;
+	}
+	
 	static int peakele(int arr[],int n) {
 		   
 	      if(n==0) { return 0; }
@@ -37,6 +57,7 @@ public class peakElement {
 	   return 0;
 
 }
+
    }
 
 
